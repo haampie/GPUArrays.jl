2d3eba3def8872bc74153cececda1d35a066e499
janEbert <janpublicebert@posteo.net>
2020-01-25 16:01:05 +0100

    Add Base.clamp!

---
+++ src/host/math.jl
@@ -0,0 +1,12 @@
+import Base.clamp!
+
+
+function Base.clamp!(A::AbstractGPUArray, low, high)
+    function kernel(state, A, low, high)
+        I = @cartesianidx A state
+        A[I...] = clamp(A[I...], low, high)
+        return
+    end
+    gpu_call(kernel, A, (A, low, high))
+    return A
+end
